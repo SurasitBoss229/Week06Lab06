0229a1daec4b4ca7f1b8895565c711c8a8d65a50
Surasit <surasitboss229@gmail.com>
2017-09-21 16:45:58 +0700

    W06P25

--- Week06Lab06/Week06Lab06/Program.cs
+++ Week06Lab06/Week06Lab06/Program.cs
@@ -8,8 +8,28 @@ namespace Week06Lab06
 {
     class Program
     {
+        static void EvenOddNumber(double N)
+        {
+            string x;
+            if ((N % 2) > 0)
+            {
+                x = ("This is Odd Number");
+            }
+            else
+            {
+                x = ("This is Even Number");  
+            }
+            Console.WriteLine("{0}", x);
+            return;
+        }
+
         static void Main(string[] args)
         {
+            double N;
+            Console.Write("Enter a number : ");
+            N = double.Parse(Console.ReadLine());
+            EvenOddNumber(N);
+            Console.ReadLine();
         }
     }
 }
