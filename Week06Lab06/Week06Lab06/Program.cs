af7f43142378239b575bed0bf17be985185dca2b
Surasit <surasitboss229@gmail.com>
2017-09-21 17:47:59 +0700

    W06P28

--- Week06Lab06/Week06Lab06/Program.cs
+++ Week06Lab06/Week06Lab06/Program.cs
@@ -8,8 +8,24 @@ namespace Week06Lab06
 {
     class Program
     {
+        static void trapezoid(double A, double B, double C)
+        {
+            double D;
+            D = ((1 / 2)*(A + B)*C);
+            Console.Write("Area of the trapezoid is : {0}" , D);
+        }
+
         static void Main(string[] args)
         {
+            double A, B, C;
+            Console.Write("Enter the Parallel 1 : ");
+            A = double.Parse(Console.ReadLine());
+            Console.Write("Enter the Parallel 2 : ");
+            B = double.Parse(Console.ReadLine());
+            Console.Write("Enter the High : ");
+            C = double.Parse(Console.ReadLine());
+            trapezoid(A, B, C);
+            Console.ReadLine();
         }
     }
 }
