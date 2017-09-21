8605e281b6ffccf61168b25516ee1cf6b10eb89d
Surasit <surasitboss229@gmail.com>
2017-09-21 13:06:15 +0700

    W06P17

--- Week06Lab06/Week06Lab06/Program.cs
+++ Week06Lab06/Week06Lab06/Program.cs
@@ -10,6 +10,12 @@ namespace Week06Lab06
     {
         static void Main(string[] args)
         {
+            string hello = "Hello";
+            string world = "World";
+            string helloWorld = hello + world;
+            Console.WriteLine(helloWorld);
+            string helloWorld555 = helloWorld + " " + 555;
+            Console.WriteLine(helloWorld555);
         }
     }
 }
