32e1a588e5f51eca305219eaa4642db67e8f8a5e
Surasit <surasitboss229@gmail.com>
2017-09-21 12:56:34 +0700

    W06P15

--- Week06Lab06/Week06Lab06/Program.cs
+++ Week06Lab06/Week06Lab06/Program.cs
@@ -10,6 +10,16 @@ namespace Week06Lab06
     {
         static void Main(string[] args)
         {
+            byte a = 3;
+            byte b = 5;
+
+            Console.WriteLine(a | b);
+            Console.WriteLine(a & b);
+            Console.WriteLine(a ^ b);
+            Console.WriteLine(~a & b);
+            Console.WriteLine(a << 1);
+            Console.WriteLine(a << 2);
+            Console.WriteLine(a >> 1);
         }
     }
 }
