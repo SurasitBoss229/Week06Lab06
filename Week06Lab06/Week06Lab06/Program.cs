5d7c734eff057d8f81eac824912f06cea824a923
Surasit <surasitboss229@gmail.com>
2017-09-21 18:03:53 +0700

    W06P29

--- Week06Lab06/Week06Lab06/Program.cs
+++ Week06Lab06/Week06Lab06/Program.cs
@@ -8,8 +8,20 @@ namespace Week06Lab06
 {
     class Program
     {
+        static void Moon(double X)
+        {
+            double W;
+            W = (X * 1.6);
+            Console.WriteLine("Weight on the moon is : {0} N.", W);
+        }
+
         static void Main(string[] args)
         {
+            double X;
+            Console.Write("Enter the Mass(kg) : ");
+            X = double.Parse(Console.ReadLine());
+            Moon(X);
+            Console.ReadLine();
         }
     }
 }
