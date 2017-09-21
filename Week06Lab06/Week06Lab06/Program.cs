0fea34d29c0f45292fda1cacf08ba8576606228a
Surasit <surasitboss229@gmail.com>
2017-09-21 12:37:50 +0700

    W06P13

--- Week06Lab06/Week06Lab06/Program.cs
+++ Week06Lab06/Week06Lab06/Program.cs
@@ -10,6 +10,14 @@ namespace Week06Lab06
     {
         static void Main(string[] args)
         {
+
+            int x = 10, y = 5;
+            Console.WriteLine("x > y : " + (x > y));
+            Console.WriteLine("x < y : " + (x < y));
+            Console.WriteLine("x >= y : " + (x >= y));
+            Console.WriteLine("x <= y : " + (x <= y));
+            Console.WriteLine("x == y : " + (x == y));
+            Console.WriteLine("x != y : " + (x != y));
         }
     }
 }
